b8ca27009da037cdbfe4b522d4908375ab685019
TejalD123 <tejaldalvi290@gmail.com>
2026-02-24 20:50:32 +0530

    files added

---
+++ app.js
@@ -0,0 +1,9 @@
+const navlist = document.getElementById("navlist");
+const hamburger = document.getElementById("hamburger");
+
+//click event
+
+hamburger.addEventListener("click",()=>{
+
+    navlist.classList.toggle("navlist-active")
+})
